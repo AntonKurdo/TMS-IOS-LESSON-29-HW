d1c66f4ba83549dc3e75c98e1c932532ee67669a
mac <mac@macs-MacBook-Pro.local>
2024-02-28 13:22:48 +0300

    homework

---
+++ TMS-IOS-LESSON-29-HW/ViewControllers/ViewController.swift
@@ -0,0 +1,57 @@
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    var spinner = SpinnerViewController()
+    
+    let tableView = UITableView()
+    
+    var users: Users = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSpinner()
+        setupTable()
+        fetchUsers()
+    }
+    
+    private func fetchUsers() {
+        view.addSubview(spinner.view)
+        AF.request("https://jsonplaceholder.typicode.com/users", method: .get ).responseDecodable(of: Users.self) { response in
+            switch response.result {
+            case .success(let data):
+                self.users = data
+            case .failure(let error):
+                print(error)
+            }
+            self.spinner.willMove(toParent: nil)
+            self.spinner.view.removeFromSuperview()
+            self.spinner.removeFromParent()
+        }
+    }
+    
+    private func setupSpinner() {
+        spinner.view.frame = view.frame
+    }
+    
+    private func setupTable() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+        ])
+    }
+}
